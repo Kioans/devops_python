f98c37a3c74446dedfdfd0f450d3ef974d4e94a0
Ivan K <gnaoik@gmail.com>
2020-01-26 19:20:07 +0300

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,19 +1,20 @@
-mx=1
-v=0
-s = input()
-s.lower() #приводим слова к нижнему регистру
-a = s.split() # создаём список из введённой строки
-dictionary = {}
+while True:
+    mx = 1
+    v = 0
+    s = input("Введите строку: ")
+    s.lower() #приводим слова к нижнему регистру
+    a = s.split() # создаём список из введённой строки
+    dictionary = {}
 
-for i in a:
-    if dictionary.get(i) is None: #если в словаре нет ключа i то добавляем его со значением 1
-        dictionary.update({i: 1})
-    else: # если в словаре есть ключ, то увеличиваем значение на 1
-        v = dictionary.get(i) + 1
-        dictionary.update({i: v})
-    if mx < v:
-        mx = v
+    for i in a:
+        if dictionary.get(i) is None: #если в словаре нет ключа i то добавляем его со значением 1
+            dictionary.update({i: 1})
+        else: # если в словаре есть ключ, то увеличиваем значение на 1
+            v = dictionary.get(i) + 1
+            dictionary.update({i: v})
+        if mx < v:
+            mx = v
 
-for key,value in dictionary.items():
-    if value == mx:
-        print(f"{value} - {key}")
+    for key,value in dictionary.items():
+        if value == mx:
+            print(f"{value} - {key}")
