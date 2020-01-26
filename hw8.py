2c64320b146586e5365e9be386ffa9b22ccdfc8a
Ivan K <gnaoik@gmail.com>
2020-01-26 20:56:05 +0300

    Update hw8.py

--- hw8.py
+++ hw8.py
@@ -11,15 +11,14 @@ def stringToNumber():
     s = input()
     if s == "cancel":
         print("Bye")
-        return ()
     else:
         try:
             i = int(s)
             x = int(i/2) if i % 2 == 0 else  i * 3 + 1
             print(x)
+            stringToNumber()
         except:
             print("Не удалось преобразовать введенный текст в число.")
-        finally:
             stringToNumber()
 
 stringToNumber()
