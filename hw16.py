2cddc0334c9fceda71ee3b64ff5db1449cf98d52
Ivan K <gnaoik@gmail.com>
2020-02-04 22:52:00 +0300

    Update hw16.py
    получение gps координат стало методом get_location, на вход может принимать 2 значения: Широту, Долготу.  вывод  gps информации на экран стало методом print_location

--- hw16.py
+++ hw16.py
@@ -12,24 +12,35 @@ Goggle Maps URL: https://www.google.com/maps/search/?api=1&query=60.016666666666
 '''
 from opencage.geocoder import OpenCageGeocode
 
+def get_location(lat='X', lng='X'):
+    key = '2ead87ca7a984517ba94f2b73a70edc5'
+    file_gps = "hw16.txt"
+    listGps = []
+    geocoder = OpenCageGeocode(key)
+    if lat != 'X' and lng != 'X':
+        listGps.append(str(lat + ', ' + lng))
+        results = geocoder.reverse_geocode(lat, lng)
+        print_location(results)
+    else:
+        with open(file_gps,'r') as file:
+            listGps = file.readlines()
+        for i in listGps:
+            try:
+                latlng = i.split(', ')
+                results = geocoder.reverse_geocode(latlng[0], latlng[1])
+            except:
+                print("Не верно введённые данные")
+                break
+            print_location(results)
 
-key = '2ead87ca7a984517ba94f2b73a70edc5'
-geocoder = OpenCageGeocode(key)
-listGps = []
-with open("hw16.txt",'r') as file:
-    listGps = file.readlines()
-for i in listGps:
-    try:
-        latlng = i.split(', ')
-        results = geocoder.reverse_geocode(latlng[0], latlng[1])
-    except:
-        print("Не верно введённые данные")
-        break
-    print("Input data: ", i.strip('\n'))
+def print_location(results):
+    print("Input data: ", results[0]['geometry']['lat'], results[0]['geometry']['lng'])
     print("Output data: ")
     print("Location: ", results[0]['formatted'])
-    print("Goggle Maps URL: https://www.google.com/maps/search/?api=1&query=" + latlng[0] + ',' + latlng[1])
+    lat = str(results[0]['geometry']['lat'])
+    lng = str(results[0]['geometry']['lng'])
+    print("Goggle Maps URL: https://www.google.com/maps/search/?api=1&query=" + lat + ',' + lng)
 
 
-
-
+if __name__ == '__main__':
+    get_location()
